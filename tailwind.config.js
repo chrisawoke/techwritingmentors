b5978f0178c3e918a4128c18a3b7dd660f310790
Chris Awoke <uchechristian27@gmail.com>
2024-03-03 19:08:51 +0100

    latest update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,37 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
+      fontSize: {
+        md: "1.6rem",
+        lg: ["2.2rem", "1.3"],
+        "5xl": ["8rem", "1.2"],
+      },
+      colors: {
+        backGround: "#FAFCFF",
+        LightBlue: "#E9F1FA",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        brightBlue: "#00ABE4",
+      },
+      // spacing: {
+      //   0: "0",
+      //   1: "0.4rem",
+      //   2: "0.8rem",
+      //   3: "1.2rem",
+      //   4: "1.6rem",
+      //   5: "2.3rem",
+      //   6: "3.2rem",
+      // },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
