05a0bef829d24c7ddd2ff42bb7f84c10daaf28d5
Chris Awoke <uchechristian27@gmail.com>
2024-03-03 18:31:56 +0100

    Create index.js

---
+++ constants/index.js
@@ -0,0 +1,52 @@
+export const navLinks = [
+  {
+    id: 2,
+    link: "/about",
+    title: "About",
+  },
+  {
+    id: 3,
+    link: "/courses",
+    title: "Courses",
+  },
+  {
+    id: 4,
+    link: "/resources",
+    title: "Resources",
+  },
+  {
+    id: 5,
+    link: "/contact",
+    title: "Contact",
+  },
+];
+
+// Everything Footer
+export const footerLinks = [
+  {
+    id: 1,
+    title: "Useful Links",
+    links: [
+      {
+        id: 1,
+        title: "Resources",
+        link: "/resources",
+      },
+      {
+        id: 2,
+        title: "Contact",
+        link: "/contact",
+      },
+      {
+        id: 3,
+        title: "Blog",
+        link: "/blog",
+      },
+      {
+        id: 4,
+        title: "About",
+        link: "/about",
+      },
+    ],
+  },
+];
